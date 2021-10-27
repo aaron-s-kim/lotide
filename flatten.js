a9b5135f0bac2dfe8d268a221a4238cc40260465
Aaron K <tonberryspawn@gmail.com>
2021-10-26 23:47:16 -0700

    flatten.js

---
+++ flatten.js
@@ -0,0 +1,40 @@
+// Create function flatten which will take in an array containing elements
+// including nested arrays of elements, and return a "flattened" version of the array.
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const eqArrays = function(a1, a2) {
+  if (a1.length === a2.length) {
+    for (let i = 0; i < a1.length; i++) {
+      if (a1[i] !== a2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const flatten = function(arr) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        newArr.push(arr[i][j]);
+      }
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return console.log(newArr);
+};
+
+// Test Output
+flatten([1, 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
